fef8a682622db6c903158be50da448f38522a37a
Daniel Pereira Joaquim <dann.pereira.j@gmail.com>
2024-09-19 11:26:47 -0300

    feat/ PWA adicionado

--- trabalho-final/static/sw.js
+++ trabalho-final/static/sw.js
@@ -1,3 +1,16 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('cronometro-cache-v1').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/static/style.css',
+          '/static/script.js',
+          '/static/img/cronometro.png',
+        ]);
+      })
+    );
+  });
+
 self.addEventListener("fetch", event => {
     console.log('teste')
 })
